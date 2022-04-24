615668b1a456553a29ed5102dc06c66172f2b8f4
Boukalli Abdelmonaim <63216504+Akschan@users.noreply.github.com>
2022-04-24 22:35:05 +0000

    Create Train_Model.py

---
+++ Train_Model.py
@@ -0,0 +1,15 @@
+from Model.py import * # import our function
+
+xtrain, xvalidation, xtest, ytrain, yvalidation, ytest, input_shape = Make_datasets(0.25, 0.2) #make train validation test sets and input shape
+                                                                                               #(test is 25% of data and validation is 20%)
+  
+  
+model = Model(input_shape)
+
+optimizer = keras.optimizers.Adam(learning_rate = 0.0001) # useing adam optimizer with 0.0001 learning rate
+
+model.compile(optimizer = optimizer, loss = "sparse_categorical_crossentropy", metrics = ['accuracy']) #compiling the model
+
+history = model.fit(xtrain, ytrain, validation_data = (xvalidation,yvalidation), batch_size=32, epochs=50) # train the model
+
+plot_history(history) #plot & save the plot as CNN.png
